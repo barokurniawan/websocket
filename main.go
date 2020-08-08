2b9459f85a5f39e2b83f892f6353eefbe7e0cb5b
barokurniawan <kurniawan9411n@gmail.com>
2020-08-08 18:50:12 +0700

    make it more clear

--- main.go
+++ main.go
@@ -2,75 +2,19 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"net/http"
-	"strings"
 
-	"github.com/barokurniawan/websocket/config"
-	"github.com/barokurniawan/websocket/helper"
-	"github.com/barokurniawan/websocket/sockethandler"
-	"github.com/barokurniawan/websocket/structure"
-	"github.com/gorilla/websocket"
+	"github.com/barokurniawan/websocket/context"
+	"github.com/barokurniawan/websocket/controller"
 )
 
 func main() {
-	AppConfig := config.App{
-		Port:    ":3001",
-		Address: "127.0.0.1",
-	}
+	context.LoadConfig()
 
-	allowedOrigin := []string{"localhost:3001", "103.93.161.49:3100"}
+	http.HandleFunc("/", controller.IndexHandler)
+	http.HandleFunc("/other", controller.OtherHandler)
+	http.HandleFunc("/socket", controller.SocketHandler)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		content, err := ioutil.ReadFile("index.html")
-		if err != nil {
-			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "%s", content)
-	})
-
-	http.HandleFunc("/other", func(w http.ResponseWriter, r *http.Request) {
-		content, err := ioutil.ReadFile("other.html")
-		if err != nil {
-			http.Error(w, "Could not open requested file", http.StatusInternalServerError)
-			return
-		}
-
-		fmt.Fprintf(w, "%s", content)
-	})
-
-	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Origin") == "" {
-			http.Error(w, "Invalid origin!", http.StatusBadRequest)
-			return
-		}
-
-		originHost := strings.Split(r.Header.Get("Origin"), "://")
-		host := originHost[1]
-		if exists, _ := helper.InArray(host, allowedOrigin); exists == false {
-			http.Error(w, "Invalid origin!", http.StatusForbidden)
-			return
-		}
-
-		channel, ok := r.URL.Query()["channel"]
-		if !ok || len(channel[0]) < 1 {
-			http.Error(w, "missing chanel parameter", http.StatusBadRequest)
-			return
-		}
-
-		currentGorillaConn, err := websocket.Upgrade(w, r, w.Header(), 1024, 1024)
-		if err != nil {
-			http.Error(w, "Could not open websocket connection", http.StatusBadRequest)
-		}
-
-		currentConn := structure.WebSocketConnection{Conn: currentGorillaConn, Channel: string(channel[0])}
-		sockethandler.Connections = append(sockethandler.Connections, &currentConn)
-
-		go sockethandler.HandleIO(&currentConn, sockethandler.Connections)
-	})
-
-	fmt.Println("Server starting at " + AppConfig.Address + AppConfig.Port)
-	http.ListenAndServe(AppConfig.Address+AppConfig.Port, nil)
+	fmt.Println("Server starting at " + context.Config.Address + context.Config.Port)
+	http.ListenAndServe(context.Config.Address+context.Config.Port, nil)
 }
