49fe33cd11d557b84093b557a460cbb2b81b795d
barokurniawan <kurniawan9411n@gmail.com>
2020-08-08 17:53:09 +0700

    improve main

--- main.go
+++ main.go
@@ -4,13 +4,23 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 
+	"github.com/barokurniawan/websocket/config"
+	"github.com/barokurniawan/websocket/helper"
 	"github.com/barokurniawan/websocket/sockethandler"
 	"github.com/barokurniawan/websocket/structure"
 	"github.com/gorilla/websocket"
 )
 
 func main() {
+	AppConfig := config.App{
+		Port:    ":3001",
+		Address: "127.0.0.1",
+	}
+
+	allowedOrigin := []string{"localhost:3001", "103.93.161.49:3100"}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		content, err := ioutil.ReadFile("index.html")
 		if err != nil {
@@ -32,6 +42,18 @@ func main() {
 	})
 
 	http.HandleFunc("/socket", func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Origin") == "" {
+			http.Error(w, "Invalid origin!", http.StatusBadRequest)
+			return
+		}
+
+		originHost := strings.Split(r.Header.Get("Origin"), "://")
+		host := originHost[1]
+		if exists, _ := helper.InArray(host, allowedOrigin); exists == false {
+			http.Error(w, "Invalid origin!", http.StatusForbidden)
+			return
+		}
+
 		channel, ok := r.URL.Query()["channel"]
 		if !ok || len(channel[0]) < 1 {
 			http.Error(w, "missing chanel parameter", http.StatusBadRequest)
@@ -49,6 +71,6 @@ func main() {
 		go sockethandler.HandleIO(&currentConn, sockethandler.Connections)
 	})
 
-	fmt.Println("Server starting at :3001")
-	http.ListenAndServe(":3100", nil)
+	fmt.Println("Server starting at " + AppConfig.Address + AppConfig.Port)
+	http.ListenAndServe(AppConfig.Address+AppConfig.Port, nil)
 }
